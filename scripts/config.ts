cfdb138512e9001a3424d164db42c69e563a0799
Milesq <wmilosz88@gmail.com>
2021-02-13 20:21:53 +0100

    feat: add config

---
+++ scripts/config.ts
@@ -0,0 +1,7 @@
+export default {
+  API: 'https://jw.org',
+  SELECTOR: {
+    READ_BIBLE:
+      '#content > div.homeSectionContainer.homeWhatsNewGrid > div.homeWhatsNewItem.first.home-onlineBible.presentationIntent-excludeDefault > div > div.syn-body.sqr > h3 > a',
+  },
+}
