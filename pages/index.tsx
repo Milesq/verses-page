e44d27f30a1772bde52b7996d60e5b49780b67e4
Milesq <wmilosz88@gmail.com>
2021-02-13 12:27:43 +0100

    chore(eslint): improve eslint config

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,10 @@ import { inc } from 'ramda'
 
 const Home: FC = () => {
   const [count, setCount] = useState(0)
+  const x = {
+    s: 3,
+  }
+  console.log(x)
 
   return (
     <div className="bg-gray-500 text-white w-11" onClick={() => setCount(inc)}>
