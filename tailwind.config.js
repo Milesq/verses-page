c6bcabddf1332fb5a4ea54ff15e5b8f73968e527
Milesq <wmilosz88@gmail.com>
2021-02-16 12:22:20 +0100

    feat: add verse title with aquire font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        aquire: ['Aquire', 'sans-serif'],
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
