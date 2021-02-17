3c31ee14e8f9758c990ffbfbbace645388f24ba5
Milesq <wmilosz88@gmail.com>
2021-02-17 20:41:24 +0100

    feat: add chapter input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import type { FC } from 'react'
+import Select from 'react-select'
 import NavBar from '../components/NavBar'
+import allBooks from '../scripts/books.json'
+import BookData from '../scripts/BookData'
 
 const Home: FC = () => {
-  const { locale, locales } = useRouter()
+  const { locale } = useRouter()
+  const currentLang: BookData[] = allBooks[locale]
+
+  const currentBooks = currentLang.map(({ name, path }) => ({
+    label: name,
+    value: path,
+  }))
 
   return (
     <>
@@ -12,7 +21,39 @@ const Home: FC = () => {
         <title>Verses - generowanie plansz z wersetami</title>
       </Head>
       <NavBar />
-      <div style={{ height: '200vh' }}>hello</div>
+      <div className="mt-4 container px-8 lg:px-52 xl:px-96 mx-auto">
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label htmlFor="react-select-book" />
+        <Select
+          aria-label=""
+          placeholder="Wyszukaj księgę"
+          id="react-select-book"
+          instanceId="react-select-book"
+          options={currentBooks}
+        />
+
+        <input
+          placeholder="Rozdział i werset (Przykład 30:2)"
+          className="
+            w-full
+            appearance-none
+            my-3
+            border-solid
+            border
+            border-input
+            hover:border-input-darken
+            rounded
+            py-2
+            px-3
+            text-gray-700
+            leading-tight
+            focus:outline-none
+            focus:shadow-outline
+          "
+          id="username"
+          type="text"
+        />
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
       fontFamily: {
         aquire: ['Aquire', 'sans-serif'],
       },
+      colors: {
+        jw: {
+          DEFAULT: '#4a6da7',
+          'darken-1': '#275197',
+          'darken-2': '#1d3254',
+        },
+        input: {
+          DEFAULT: '#ccc',
+          darken: '#b3b3b3',
+        },
+      },
     },
   },
 }
