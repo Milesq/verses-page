494bd9060c16b501681f02d40c3faa67fd23e46c
Milesq <wmilosz88@gmail.com>
2021-02-17 23:05:51 +0100

    feat: add generate button

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 import Head from 'next/head'
 import type { FC } from 'react'
 import Select from 'react-select'
+import Ripples from 'react-ripples'
 import NavBar from '../components/NavBar'
 import allBooks from '../scripts/books.json'
 import BookData from '../scripts/BookData'
@@ -53,6 +54,36 @@ const Home: FC = () => {
           id="username"
           type="text"
         />
+
+        <div
+          className="
+            float-right
+            rounded-full
+            overflow-hidden
+          "
+        >
+          <Ripples>
+            <button
+              className="
+              transition
+              duration-150
+              bg-green-600
+              hover:bg-green-500
+              shadow
+              hover:shadow-md
+              text-white
+              px-3 py-2
+              rounded-full
+              focus:outline-none
+            "
+              type="submit"
+            >
+              Generuj
+            </button>
+          </Ripples>
+        </div>
+
+        <div className="clear-both" />
       </div>
     </>
   )
