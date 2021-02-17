c52debc610e0e5cd01d385c5b6803f3dccc31ca8
Milesq <wmilosz88@gmail.com>
2021-02-17 17:58:35 +0100

    fix: add margin for fixed element

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -20,32 +20,36 @@ const NavBar: FC = () => {
   }
 
   return (
-    <nav
-      className={`${
-        scroll ? 'shadow-md' : 'md:shadow'
-      } transition-shadow duration-300 h-16 fixed w-full flex justify-between items-center px-3 bg-white`}
-    >
-      <div>
-        <Image src="/verse.png" width={40} height={40} alt="Verse's Logo" />
-      </div>
+    <>
+      <nav
+        className={`${
+          scroll ? 'shadow-md' : 'md:shadow'
+        } transition-shadow duration-300 h-16 fixed w-full flex justify-between items-center px-3 bg-white`}
+      >
+        <div>
+          <Image src="/verse.png" width={40} height={40} alt="Verse's Logo" />
+        </div>
 
-      <span className="text-4xl font-aquire select-none">Verse</span>
+        <span className="text-4xl font-aquire select-none">Verse</span>
 
-      <div>
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label htmlFor="react-select-language" />
-        <Select
-          aria-label=""
-          id="react-select-language"
-          instanceId="react-select-language"
-          className="w-20"
-          defaultValue={currentLang}
-          options={langs}
-          isSearchable={false}
-          onChange={selectLang}
-        />
-      </div>
-    </nav>
+        <div>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="react-select-language" />
+          <Select
+            aria-label=""
+            id="react-select-language"
+            instanceId="react-select-language"
+            className="w-20"
+            defaultValue={currentLang}
+            options={langs}
+            isSearchable={false}
+            onChange={selectLang}
+          />
+        </div>
+      </nav>
+
+      <div className="h-16" />
+    </>
   )
 }
 
