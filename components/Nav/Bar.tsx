6c2d72488d415de76e3869bdaa07c53a91567fa9
Milesq <wmilosz88@gmail.com>
2021-02-18 16:32:28 +0100

    feat: split navbar into 2 files

---
+++ components/Nav/Bar.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC } from 'react'
+import { useScroll } from '../../hooks'
+import NavMenu from './Menu'
+
+const NavBar: FC = () => {
+  const { y: scroll } = useScroll()
+
+  return (
+    <>
+      <nav
+        className={`${
+          scroll ? 'shadow-md' : 'md:shadow'
+        } transition-shadow duration-300 h-16 fixed w-full grid grid-cols-3 px-3 bg-white dark:bg-gray-800 dark:text-white`}
+      >
+        <div className="hidden md:flex items-center">
+          <Link href="/">
+            <div>
+              <Image
+                src="/verse.png"
+                className="navbar-logo cursor-pointer"
+                width={40}
+                height={40}
+                alt="Verse's Logo"
+              />
+            </div>
+          </Link>
+        </div>
+
+        <span className="text-4xl font-aquire select-none flex items-center justify-center">
+          <Link href="/">Verse</Link>
+        </span>
+        <div className="block md:hidden" />
+
+        <NavMenu />
+      </nav>
+
+      <div className="h-16" />
+    </>
+  )
+}
+
+export default NavBar
---
+++ components/Nav/Menu.tsx
@@ -0,0 +1,70 @@
+import Link from 'next/link'
+import { FC, useEffect, useState } from 'react'
+import Select from 'react-select'
+import Ripples from 'react-ripples'
+import { useRouter } from 'next/router'
+import ThemeSwitch from '../ThemeSwitch'
+import Hamburger from '../Hamburger'
+
+const NavBar: FC = () => {
+  const { locales, locale, ...router } = useRouter()
+  const [theme, setTheme] = useState('')
+
+  const langs = locales.map(lang => ({
+    value: lang,
+    label: lang.toUpperCase(),
+  }))
+  const currentLang = langs.find(lang => lang.value === locale)
+
+  function selectLang({ value }: { value: string }) {
+    document.cookie = `NEXT_LOCALE=${value}`
+    router.push('/', '/', { locale: value })
+  }
+
+  function changeTheme(active: boolean) {
+    document.querySelector('html').classList.toggle('dark')
+    localStorage.setItem('theme', active ? 'dark' : 'light')
+  }
+
+  useEffect(() => {
+    const selectedTheme = localStorage.getItem('theme')
+    if (selectedTheme === 'dark') {
+      changeTheme(true)
+    }
+
+    setTheme(selectedTheme)
+  }, [])
+
+  return (
+    <>
+      <div className="block md:hidden">
+        <Hamburger type="spring" />
+      </div>
+
+      <div className="hidden md:flex items-center justify-end">
+        <span className="pr-4">
+          <Ripples className="p-2">
+            <Link href="/issue">Zgłoś błąd</Link>
+          </Ripples>
+        </span>
+        <ThemeSwitch onChange={changeTheme} value={theme === 'dark'} />
+        <div className="pl-4">
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="react-select-language" />
+          <Select
+            aria-label=""
+            id="react-select-language"
+            instanceId="react-select-language"
+            className="w-20"
+            defaultValue={currentLang}
+            options={langs}
+            isSearchable={false}
+            onChange={selectLang}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default NavBar
