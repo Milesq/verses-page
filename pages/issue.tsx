bc04437e4b096b9fbbbe57aad7336afc37634038
Milesq <wmilosz88@gmail.com>
2021-02-21 00:01:40 +0100

    feat: controll issue form

--- pages/issue.tsx
+++ pages/issue.tsx
@@ -1,9 +1,19 @@
 import Head from 'next/head'
 import type { FC } from 'react'
+import { useForm } from 'react-hook-form'
 import Button from '../components/Button'
 
+interface Inputs {
+  contact?: string
+  content: string
+}
+
 const Issue: FC = () => {
-  console.log('Hello, World!')
+  const { register, handleSubmit, errors } = useForm<Inputs>()
+
+  function sendCaution(data: Inputs) {
+    console.log(data)
+  }
 
   return (
     <>
@@ -13,15 +23,35 @@ const Issue: FC = () => {
 
       <h3 className="dark:text-gray-50">Zgłoś błąd, poprawkę lub sugestię</h3>
 
-      <input
-        placeholder="Dane konktaktowe (opcjonalnie)"
-        className="pretty-input"
-        type="text"
-      />
+      {errors.contact && (
+        <div className="text-red-500">
+          Jeśli chcesz podać dane kontaktowe, niech będą poprawne
+        </div>
+      )}
 
-      <textarea className="pretty-input" rows={10} placeholder="Informacje" />
+      <form onSubmit={handleSubmit(sendCaution)}>
+        <input
+          name="contact"
+          ref={register({ minLength: 5 })}
+          placeholder="Dane konktaktowe (opcjonalnie)"
+          className="pretty-input"
+          type="text"
+        />
 
-      <Button>Zgłoś</Button>
+        {errors.content && (
+          <div className="text-red-500">Wpisz uwagę zanim ją wyślesz</div>
+        )}
+
+        <textarea
+          name="content"
+          ref={register({ required: true, minLength: 3 })}
+          className="pretty-input"
+          rows={10}
+          placeholder="Informacje"
+        />
+
+        <Button>Zgłoś</Button>
+      </form>
     </>
   )
 }
