a9a366dae279e290a5e70d15c3d7f49302fcebca
Milesq <wmilosz88@gmail.com>
2021-02-20 11:34:56 +0100

    feat: add html for issue page

--- pages/issue.tsx
+++ pages/issue.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import type { FC } from 'react'
+import Button from '../components/Button'
 
 const Issue: FC = () => {
   console.log('Hello, World!')
@@ -9,6 +10,18 @@ const Issue: FC = () => {
       <Head>
         <title>Zgłaszanie błędów</title>
       </Head>
+
+      <h3 className="dark:text-gray-50">Zgłoś błąd, poprawkę lub sugestię</h3>
+
+      <input
+        placeholder="Dane konktaktowe (opcjonalnie)"
+        className="pretty-input"
+        type="text"
+      />
+
+      <textarea className="pretty-input" rows={10} placeholder="Informacje" />
+
+      <Button>Zgłoś</Button>
     </>
   )
 }
