8d718a11b522cc6660b8c4e49cb87cd3f8ac3764
Milesq <wmilosz88@gmail.com>
2021-02-20 11:23:27 +0100

    refactor: extract input styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,78 +21,57 @@ const Home: FC = () => {
       <Head>
         <title>Verses - generowanie plansz z wersetami</title>
       </Head>
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label htmlFor="react-select-book" />
-        <Select
-          styles={reactSelectThemedStyle}
-          aria-label=""
-          placeholder="Wyszukaj księgę"
-          id="react-select-book"
-          instanceId="react-select-book"
-          options={currentBooks}
-        />
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor="react-select-book" />
+      <Select
+        styles={reactSelectThemedStyle}
+        aria-label=""
+        placeholder="Wyszukaj księgę"
+        id="react-select-book"
+        instanceId="react-select-book"
+        options={currentBooks}
+      />
 
-        <input
-          placeholder="Rozdział i werset (Przykład 30:2)"
-          className="
-            w-full
-            appearance-none
-            my-3
-            border-solid
-            border
-            border-input
-            hover:border-input-darken
-            rounded
-            py-2
-            px-3
-            text-gray-700
-            leading-tight
-            focus:outline-none
-            focus:ring
-            focus:shadow-outline
+      <input
+        placeholder="Rozdział i werset (Przykład 30:2)"
+        className="pretty-input"
+        autoComplete="off"
+        type="text"
+      />
 
-            dark:text-gray-50
-            dark:bg-gray-700
-            dark:border-gray-500
-            dark:hover:border-input-darkhover
-          "
-          autoComplete="off"
-          type="text"
-        />
+      <div
+        className="
+          float-right
+          rounded-full
+          overflow-hidden
+        "
+      >
+        <Ripples>
+          <button
+            className="
+              transition
+              duration-150
+              bg-green-600
+              hover:bg-green-500
+              dark:hover:bg-green-700
+              shadow
+              hover:shadow-md
+              text-white
+              px-3 py-2
+              rounded-full
+              focus:outline-none
+            "
+            type="submit"
+          >
+            Generuj
+          </button>
+        </Ripples>
+      </div>
+      <div style={{ height: '200vh' }} className="dark:text-gray-50">
+        ok
+      </div>
 
-        <div
-          className="
-            float-right
-            rounded-full
-            overflow-hidden
-          "
-        >
-          <Ripples>
-            <button
-              className="
-                transition
-                duration-150
-                bg-green-600
-                hover:bg-green-500
-                dark:hover:bg-green-700
-                shadow
-                hover:shadow-md
-                text-white
-                px-3 py-2
-                rounded-full
-                focus:outline-none
-              "
-              type="submit"
-            >
-              Generuj
-            </button>
-          </Ripples>
-        </div>
-        <div style={{ height: '200vh' }} className="dark:text-gray-50">
-          ok
-        </div>
-
-        <div className="clear-both" />
+      <div className="clear-both" />
     </>
   )
 }
