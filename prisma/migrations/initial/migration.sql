1d49a0c2bb72419acb4382ee004336370b4b0575
Milesq <wmilosz88@gmail.com>
2021-02-20 20:16:45 +0100

    chore: add init migration

---
+++ prisma/migrations/initial/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Caution` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `contact` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
