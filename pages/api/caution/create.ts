607a53c8fad54af0debeb661f46128d4da6a2300
Milesq <wmilosz88@gmail.com>
2021-02-21 10:35:18 +0100

    feat: add api rate limit

--- pages/api/caution/create.ts
+++ pages/api/caution/create.ts
@@ -1,5 +1,8 @@
+/* eslint-disable consistent-return */
 import type { NowRequest, NowResponse } from '@vercel/node'
 import { PrismaClient } from '@prisma/client'
+import { serialize } from 'cookie'
+import ms from 'ms'
 import {
   optional,
   string,
@@ -10,6 +13,44 @@ import {
 } from '@typeofweb/schema'
 import { parseJsonSafe } from '../../../utils'
 
+interface RateLimitOptions {
+  times: number
+  per: string
+}
+
+function rateLimit(
+  req: NowRequest,
+  res: NowResponse,
+  { times, per }: RateLimitOptions
+) {
+  const perMs = ms(per)
+  let sentCautions = parseInt(req.cookies.sentCautions, 10) || 0
+  sentCautions += 1
+
+  if (sentCautions > times) {
+    const error = `You can report only 10 cautions in ${ms(perMs, {
+      long: true,
+    })}`
+
+    res.status(429).json({ error })
+    return false
+  }
+
+  const expires = new Date(new Date().getTime() + perMs)
+  const sentCautionsCookie = serialize(
+    'sentCautions',
+    sentCautions.toString(),
+    {
+      httpOnly: true,
+      expires,
+      path: '/',
+    }
+  )
+
+  res.setHeader('Set-Cookie', sentCautionsCookie)
+  return true
+}
+
 const payloadSchema = object({
   contact: optional(string()),
   content: string(),
@@ -18,6 +59,15 @@ const payloadSchema = object({
 const validator = validate(payloadSchema)
 
 export default async (req: NowRequest, res: NowResponse) => {
+  const inQuote = rateLimit(req, res, {
+    times: 10,
+    per: '30m',
+  })
+
+  if (!inQuote) {
+    return
+  }
+
   const body = parseJsonSafe<TypeOf<typeof payloadSchema>>(req.body)
   if (body === null) {
     return res.status(400).send({ error: 'The body is not valid JSON' })
