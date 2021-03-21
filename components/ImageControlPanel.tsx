5c191541113b01dbd6f75cc0776d1a56d881d3ea
Milesq <wmilosz88@gmail.com>
2021-03-21 11:40:12 +0100

    feat: show image with shadow

--- components/ImageControlPanel.tsx
+++ components/ImageControlPanel.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { PropsWithChildren } from 'react'
 
 export interface Option {
@@ -12,10 +13,34 @@ export interface ControlPanelProps<T extends Record<string, Option>> {
   controls: T & Record<string, Option>
 }
 
-function ImageControlPanel<T extends Record<string, Option>>(
-  props: PropsWithChildren<ControlPanelProps<T>>
-) {
-  return <div>Hello World!</div>
+function ImageControlPanel<T extends Record<string, Option>>({
+  controls,
+  getImageRef,
+}: PropsWithChildren<ControlPanelProps<T>>) {
+  const data = Object.fromEntries(
+    Object.entries(controls).map(([name]) => [name, 'sd'])
+  )
+
+  return (
+    <>
+      <img
+        src={getImageRef(data)}
+        alt="Board with the verse"
+        className="
+          w-3/4
+          mx-auto
+          mt-8
+          rounded
+          shadow
+
+          hover:shadow-2xl
+          transition-shadow
+          duration-500
+          ease-out
+        "
+      />
+    </>
+  )
 }
 
 export default ImageControlPanel
