0aa409d8aed4298140ba4adaa560a624309f1ae9
Milesq <wmilosz88@gmail.com>
2021-11-10 20:00:33 +0100

    feat: use fiber and move handlers to `routes` directory

--- create-board/main.go
+++ create-board/main.go
@@ -3,52 +3,20 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"net/http"
+	"image"
 	"os"
 	"strings"
 
-	"github.com/milesq/verses-page/create-board/utils"
+	"github.com/gofiber/fiber/v2"
+	"github.com/milesq/verses-page/create-board/routes"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	params := r.URL.Query()
-	if !utils.ParamExists(params, "title") || !utils.ParamExists(params, "verse") {
-		fmt.Fprint(w, "You must pass both, title and verse")
-		return
-	}
-
-	title, verse := params["title"][0], params["verse"][0]
-
-	if len(title) > 100 || len(verse) > 2_000 {
-		fmt.Fprint(w, "The title or verse is too long")
-		return
-	}
-
-	templateName := "hd"
-
-	if utils.ParamExists(params, "quality") {
-		quality := params["quality"]
-		templateName = quality[0]
-	}
-
-	template, templateExists := templates[templateName]
-
-	if !templateExists {
-		fmt.Fprint(w, "unknown quality")
-		return
-	}
-
-	dc, err := utils.DrawText(template, title, verse)
-	if err != nil {
-		fmt.Fprint(w, "Cannot draw text")
-		return
-	}
-
-	dc.EncodePNG(w)
-}
+var templates map[string]image.Image
 
 func main() {
+	templates = loadTemplates()
 	port := "80"
+	app := fiber.New()
 
 	if content, err := os.ReadFile("port"); err == nil {
 		port = strings.Trim(string(content), "\t\n ")
@@ -58,6 +26,8 @@ func main() {
 
 	fmt.Println("Listening at ", addr)
 
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(addr, nil)
+	app.Get("/", routes.DrawImage)
+	app.Get("/list-templates", routes.ListTemplates)
+
+	app.Listen(addr)
 }
--- create-board/routes/listTemplates.go
+++ create-board/routes/listTemplates.go
@@ -1,12 +1,9 @@
 package routes
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 )
 
 func ListTemplates(c *fiber.Ctx) error {
-	fmt.Println("das")
 	return c.JSON([]string{"template1", "template2", "template3"})
 }
