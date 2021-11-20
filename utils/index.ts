e71408d4d625d5977001252b1e4e2409a84ad36b
Milesq <wmilosz88@gmail.com>
2021-11-20 22:10:13 +0100

    feat: `templateNameToUrl`

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,3 @@
 export { default as minimizeRaw, tag as minimize } from './minimize'
 export type { default as LabeledValue } from './LabeledValue'
+export { default as templateNametoUrl } from './templateNametoUrl'
